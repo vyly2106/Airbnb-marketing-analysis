79e4fdfe01d164b02ae69736e0bca9fe4ccfc45f
Marissa21N <98909303+Marissa21N@users.noreply.github.com>
2022-02-25 12:47:04 +0100

    Working on R to fix issue with coding

--- tools/script/Dataloop.R
+++ tools/script/Dataloop.R
@@ -1,11 +1,9 @@
 #installing and loading necessary packages
 install.packages("googledrive")
 library(googledrive)
+library(dplyr)
 
-# Using looping with return values (i.e., to "save" stuff to carry on working)
-results = lapply(1:10, function(x) x*2)
-
-# To download all of Europe's listing data to R
+# To download list of European countries data to R
 data_id <-"1EuQuvvg4EIWmlRC94ixn0cQvzekYiLX6Q6KTrUfNa8U"
 drive_download(as_id(data_id), path = "AIRBNB_DATA.csv", overwrite = TRUE)
 Airbnb_links <- read.csv("AIRBNB_DATA.csv")
@@ -13,11 +11,13 @@ Airbnb_links <- read.csv("AIRBNB_DATA.csv")
 # Load specific data sets
 Airbnb_urls <- as.character(Airbnb_links$URL)
 
-Datasets <- lapply(Airbnb_urls, function(Airbnb_urls) {
-  print(paste0('Now downloading ... ', Airbnb_urls))
-  city = tolower(as.character(Airbnb_links$City[match(Airbnb_urls, Airbnb_links$URL)]))
-  res = read.csv(Airbnb_urls)
+Datasets <- lapply(Airbnb_urls, function(url) {
+  city <- tolower(as.character(Airbnb_links$City[match(url, Airbnb_links$URL)]))
+  country <- tolower(as.character(Airbnb_links$Country[match(url,Airbnb_links$URL)]))
+  print(paste('Now downloading ... ', url))
+  res <- read.csv(url)
   res$city <- city
+  res$country <- country
   return(res)
 })
 
